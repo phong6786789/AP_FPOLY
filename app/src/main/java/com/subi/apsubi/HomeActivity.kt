0829d43c5394419e12c06876092af6285a7e3016
Phong Subi <phong6786789@gmail.com>
2021-03-17 19:04:23 +0700

    ...

--- app/src/main/java/com/subi/apsubi/HomeActivity.kt
+++ app/src/main/java/com/subi/apsubi/HomeActivity.kt
@@ -4,16 +4,25 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import kotlinx.android.synthetic.main.activity_home.*
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class HomeActivity : AppCompatActivity() {
+    var token =""
+    val BASE_URL = "https://ap.poly.edu.vn/sinh-vien/bang-diem-theo-ky"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         val acct = GoogleSignIn.getLastSignedInAccount(this)
         if (acct != null) {
-            tvText.text = acct.idToken
+            token = acct.idToken.toString()
         }
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
 
     }
 }
