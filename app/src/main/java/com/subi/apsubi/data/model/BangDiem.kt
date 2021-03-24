b48b100753f52957b9b968f7d1d986f778693e24
phong6786789 <61493358+phong6786789@users.noreply.github.com>
2021-03-24 16:14:46 +0700

    done getAll data from ap

---
+++ app/src/main/java/com/subi/apsubi/data/model/BangDiem.kt
@@ -0,0 +1,13 @@
+package com.subi.apsubi.data.model
+
+data class BangDiem(
+    val stt:String,
+    val ky:String,
+    val hocKy:String,
+    val mon:String,
+    val maMon:String,
+    val maMon2:String,
+    val tinChi:String,
+    val diem:String,
+    val trangThai:String
+    )
